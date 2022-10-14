6da244dfef00804bb3b6fbcfb92bae0d0efc0383
leandro4silva <leandro94.a.s@gmail.com>
2022-10-14 18:06:44 -0300

    Deploy da api

--- dist/src/routes/games.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gamesRoutes = void 0;
-const express_1 = require("express");
-const GameController_1 = require("../controllers/GameController");
-const gamesRoutes = (0, express_1.Router)();
-exports.gamesRoutes = gamesRoutes;
-const gameController = new GameController_1.GameController();
-gamesRoutes.get("/", gameController.index);
