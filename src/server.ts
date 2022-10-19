7a070313816e47cc2b43f15acbd6ee1a675841e8
leandro4silva <leandro94.a.s@gmail.com>
2022-10-19 15:46:33 -0300

    fix bugs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import 'dotenv/config';
 import 'express-async-errors';
+
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
+
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
@@ -9,7 +13,11 @@ import { AppError } from './utils/AppError';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) =>{
     if(error instanceof AppError){
@@ -26,8 +34,6 @@ app.use((error: Error, request: Request, response: Response, next: NextFunction)
             message: "Internal server error"
         })
 }) 
-
-
 
 const PORT = process.env.PORT || 3000;
 
